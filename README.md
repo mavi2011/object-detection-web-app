70aa4b7bc48f8008affcea90a5674716de57259f
mavi2011 <e4.mavi@gmail.com>
2022-01-25 20:49:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# object-detection-web-app
+ 
---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location="pagebedroom.html";
+}
+
+function TV(){
+    window.location="pageTV.html";
+}
+
+function desk(){
+    window.location="pageDesk.html";
+}
+
+function bottles(){
+    window.location="pageBottles.html";
+}
+
+function fruitbasket(){
+        window.location="pagefruitbasket.html";
+}
